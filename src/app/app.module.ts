ac4eee236036c21c0652c51a2370623a879291d5
Jeremías Bustos <jeremias.e.bustos@gmail.com>
2025-05-11 20:09:30 -0300

    Se agregó componente Sheet-Music-List y se puso logica de lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SheetMusicListComponent } from './sheet-music-list/sheet-music-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SheetMusicListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sheet-music-list/SheetMusic.ts
@@ -0,0 +1,8 @@
+export interface SheetMusic {
+    name: string;
+    instrument: string;
+    autor: string;
+    price: number;
+    img: string;
+    clearance: boolean;
+}
